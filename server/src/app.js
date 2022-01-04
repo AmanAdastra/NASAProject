59258b891de126fc65b90d78357275f8eb1c008b
Aman Khan <amanayankhan78@gmail.com>
2022-01-04 18:33:54 +0530

    optimized build is running succesfully

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const planetRouter = require('./routes/planets/planets.router')
 
 
@@ -7,8 +8,13 @@ const app = express()
 app.use(cors({
     origin:'http://localhost:3000'
 }))
+
 app.use(express.json())
+app.use(express.static(path.join(__dirname,"..",'public')))
 app.use(planetRouter)
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname,"..",'public/index.html')
+})
 
 
 
