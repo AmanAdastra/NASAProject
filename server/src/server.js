e40d83f9ca5a8c99d4f0d26988cc63d365c38305
Aman Khan <amanayankhan78@gmail.com>
2022-01-04 17:45:10 +0530

    load planet data succesfully

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,21 @@
 const http = require('http')
+const { start } = require('repl')
 const app = require('./app')
 
+
+const {loadPlanetsData} = require('./models/planets.models')
 const PORT = 8000
 
 const server = http.createServer(app)
 
-server.listen(PORT,()=>{
-    console.log('server is listening at port',PORT)
-})
+
+async function startServer() {
+    await loadPlanetsData()
+    server.listen(PORT,()=>{
+        console.log('server is listening at port',PORT)
+    })
+
+}
+
+
+startServer()
